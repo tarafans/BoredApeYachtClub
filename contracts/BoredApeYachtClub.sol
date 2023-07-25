e438a5273eef673a8c358f1acd49218dc233ba0d
tarafans <tarafans7@gmail.com>
2023-07-25 15:17:41 -0700

    Add dependency

--- contracts/BoredApeYachtClub.sol
+++ contracts/BoredApeYachtClub.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
  * @title BoredApeYachtClub contract
